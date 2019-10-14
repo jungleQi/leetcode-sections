a08183a3a15c1294095e345a6f1fcb2132e2965d
jungle <jungle@alibaba-inc.com>
2019-10-14 16:23:28 +0800

    494 ac dp

--- section II/dynamic programming/494. Target Sum.py
+++ section II/dynamic programming/494. Target Sum.py
@@ -1,25 +1,20 @@
 import sys
 def findTargetSumWays(nums, S):
-    def _cal(nums, N, i, curSum, memo):
-        if i == N:
-            if curSum == S:
-                return 1
-            else:
-                return 0
-        elif memo[i][curSum+1000] != -sys.maxint:
-            return memo[i][curSum+1000]
-        else:
-            add = _cal(nums, N, i+1, curSum+nums[i], memo)
-            subtract =  _cal(nums, N, i + 1, curSum-nums[i], memo)
-            memo[i][curSum+1000] = add+subtract
-            return memo[i][curSum+1000]
-
     N = len(nums)
-    memo = [[-sys.maxint for _ in range(2001)] for _ in range(N)]
-    return _cal(nums, N, 0, 0, memo)
+    dp = [[0 for _ in range(2001)] for _ in range(N)]
+    dp[0][nums[0]+1000] = 1
+    dp[0][-nums[0]+1000] += 1
+    for i in range(1,N):
+        for sum in range(-1000, 1001):
+            if dp[i-1][sum+1000] > 0:
+                dp[i][sum+1000+nums[i]] += dp[i-1][sum+1000]
+                dp[i][sum+1000-nums[i]] += dp[i-1][sum+1000]
 
-nums = [2,20,24,38,44,21,45,48,30,48,14,9,21,10,46,46,12,48,12,38]
-S = 48
-#nums = [1, 1, 1, 1, 1]
-#S = 3
+    return 0 if S>1000 else dp[N-1][S+1000]
+
+
+#nums = [2,20,24,38,44,21,45,48,30,48,14,9,21,10,46,46,12,48,12,38]
+#S = 48
+nums = [1, 1, 1, 1, 1]
+S = 30000
 print(findTargetSumWays(nums, S))

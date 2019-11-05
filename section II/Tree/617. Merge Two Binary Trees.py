ef4c4c272801e59354c2fd9d0ba05144382726ad
jungle <jungle@alibaba-inc.com>
2019-11-05 21:44:16 +0800

    617 ac

--- section II/Tree/617. Merge Two Binary Trees.py
+++ section II/Tree/617. Merge Two Binary Trees.py
@@ -1,7 +1,15 @@
 import common
 
 def mergeTrees(t1, t2):
-    return 1
+    if not t1:
+        return t2
+    if not t2:
+        return t1
+
+    t1.val += t2.val
+    t1.left = mergeTrees(t1.left, t2.left)
+    t1.right = mergeTrees(t1.right, t2.right)
+    return t1
 
 
 val = [1,2,3,None,4]

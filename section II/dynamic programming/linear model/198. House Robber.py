aaa12c6f3ce6e2c820acd77910a2ade023f6c8a2
jungle <jungle@alibaba-inc.com>
2019-11-08 11:14:39 +0800

    198 ac dp

--- section II/dynamic programming/linear model/198. House Robber.py
+++ section II/dynamic programming/linear model/198. House Robber.py
@@ -1,13 +1,18 @@
+'''
+You are a professional robber planning to rob houses along a street.
+Each house has a certain amount of money stashed,
+the only constraint stopping you from robbing each of them is that adjacent houses have security system connected
+and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house,
+determine the maximum amount of money you can rob tonight without alerting the police.
+'''
+
 def rob(nums):
-    N = len(nums)
-    if N == 0: return 0
+    f1,f2 = 0,0
+    for num in nums:
+        f2, f1 = f1, max(f2+num, f1)
+    return f1
 
-    dp = [0]*(N+1)
-    dp[1] = nums[0]
-    for i in range(2, N+1):
-        dp[i] = max(dp[i-2]+nums[i-1], dp[i-1])
-
-    return dp[-1]
-
-nums = []
+nums = [1,2]
 print(rob(nums))

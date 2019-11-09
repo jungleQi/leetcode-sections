a9bbdaccbd3ea8abb4a9cdbd1eb42c8da27de729
jungle <jungle@alibaba-inc.com>
2019-11-09 11:34:28 +0800

    1143 ac dp

--- section II/dynamic programming/area model/1143. Longest Common Subsequence.py
+++ section II/dynamic programming/area model/1143. Longest Common Subsequence.py
@@ -1,30 +1,23 @@
-#coding=utf-8
+'''
+Given two strings text1 and text2, return the length of their longest common subsequence.
+
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+If there is no common subsequence, return 0.
+'''
 
 def longestCommonSubsequence(text1, text2):
-    N = len(text1)
-    M = len(text2)
-    if N == 0 or M == 0:
-        return 0
-
-    dp = [[0 for _ in range(M)] for _ in range(N)]
-    #frontier
-    dp[0][0] = 1 if text1[0] == text2[0] else 0
-    #recursive
-    for i in range(N):
-        for j in range(M):
-            if i == 0:
-                dp[i][j] = 1 if text1[0] in text2[:j+1] else 0
-                continue
-            if j == 0:
-                dp[i][j] = 1 if text2[0] in text1[:i+1] else 0
-                continue
-
-            if text1[i] == text2[j]:
-                dp[i][j] = dp[i-1][j-1] + 1
+    l1,l2 = len(text1), len(text2)
+    dp = [[0]*(l2+1) for _ in range(l1+1)]
+    for i in range(1,l1+1):
+        for j in range(1,l2+1):
+            if text1[i-1] == text2[j-1]:
+                dp[i][j] = 1+dp[i-1][j-1]
             else:
-                dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
     return dp[-1][-1]
 
-text1 ="abc"
-text2 = "def"
-print longestCommonSubsequence(text1, text2)
+text1 = "ace"
+text2 = "jjj"
+print(longestCommonSubsequence(text1, text2))

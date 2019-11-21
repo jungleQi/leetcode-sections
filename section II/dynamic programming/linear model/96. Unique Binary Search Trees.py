092d102e4b166e22dff55acc645b8ea320cb9ddf
jungle <jungle@alibaba-inc.com>
2019-11-21 11:40:03 +0800

    96 ac dp

--- section II/dynamic programming/linear model/96. Unique Binary Search Trees.py
+++ section II/dynamic programming/linear model/96. Unique Binary Search Trees.py
@@ -1,12 +1,13 @@
+'''
+Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+'''
+
 def numTrees(n):
-    if n == 0:
-        return 0
-    dp = [0]*(n+1)
-    dp[1] = 1
-    for i in range(1,n+1):
+    dp = [1]*(n+1)
+    for i in range(2, n+1):
+        dp[i] = dp[i-1]*2
         for j in range(1,i-1):
-            dp[i] += dp[j]*dp[i-j-1]
-        dp[i] += dp[i-1]*2
+            dp[i] += dp[j]*dp[i-1-j]
     return dp[-1]
 
-print(numTrees(3))
+print(numTrees(0))

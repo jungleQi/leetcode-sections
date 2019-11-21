f5f79bef8b274532bc21771e2c74ccac22765756
jungle <jungle@alibaba-inc.com>
2019-11-21 10:38:33 +0800

    1155 ac dp

--- section II/dynamic programming/knapsack model/1155. Number of Dice Rolls With Target Sum.py
+++ section II/dynamic programming/knapsack model/1155. Number of Dice Rolls With Target Sum.py
@@ -1,16 +1,21 @@
-def numRollsToTarget(d, f, target):
-    dp = [[0 for _ in range(target+1)] for _ in range(d+1)]
-    dp[0][0] = 1
+'''
+You have d dice, and each die has f faces numbered 1, 2, ..., f.
 
-    for i in range(1,d+1):
-        for j in range(1,target+1):
+Return the number of possible ways (out of f^d total ways) modulo 10^9 + 7 to roll the dice
+so the sum of the face up numbers equals target.
+'''
+
+def numRollsToTarget(d, f, target):
+    dp = [[0]*(target+1) for _ in range(d+1)]
+    dp[0][0] = 1
+    for i in range(1, d+1):
+        for j in range(i,target+1):
             for m in range(1,f+1):
                 if m<=j:
-                    dp[i][j] = dp[i][j] + dp[i-1][j-m]
+                    dp[i][j] += dp[i-1][j-m]
 
-    return dp[-1][-1]
-
-d = 1
-f = 2
-target = 3
+    return dp[-1][-1]%(10**9+7)
+d = 2
+f = 6
+target = 7
 print(numRollsToTarget(d, f, target))

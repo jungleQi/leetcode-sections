fa37448fed5e4b3b2b1abfcdd421d08f8f9e9c94
jungle <jungle@alibaba-inc.com>
2019-12-04 22:18:42 +0800

    1262 ac dp

---
+++ section II/dynamic programming/linear model/1262. Greatest Sum Divisible by Three.py
@@ -0,0 +1,14 @@
+'''
+Given an array nums of integers, we need to find the maximum possible sum of elements
+ of the array such that it is divisible by three.
+'''
+
+def maxSumDivThree(nums):
+    seen = [0,0,0]
+    for num in nums:
+        for i in seen[:]:
+            seen[(i+num)%3] = max(seen[(i+num)%3], i+num)
+    return seen[0]
+
+nums = [1,2,3,4,4]
+print(maxSumDivThree(nums))

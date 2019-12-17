dca306b38e7ef4cee0bd6c0ca6e435469b094475
jungle <jungle@alibaba-inc.com>
2019-12-17 16:41:39 +0800

    239 sliding window

---
+++ schedule/week-1/slide window/239. Sliding Window Maximum.py
@@ -0,0 +1,32 @@
+'''
+Given an array nums, there is a sliding window of size k which is moving from the very
+left of the array to the very right. You can only see the k numbers in the window.
+Each time the sliding window moves right by one position. Return the max sliding window.
+
+Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+Output: [3,3,5,5,6,7]
+'''
+
+# Keep indexes of good candidates in deque d. The indexes in d are from the current window,
+# they're increasing, and their corresponding nums are decreasing.
+# Then the first deque element is the index of the largest window value
+
+import collections
+def maxSlidingWindow(nums, k):
+    deq = collections.deque()
+    out = []
+
+    for i,n in enumerate(nums):
+        while deq and nums[deq[-1]]<n:
+            deq.pop()
+
+        deq += i,
+        if deq[0] == i-k:
+            deq.popleft()
+        if i>=k-1:
+            out += nums[deq[0]],
+    return out
+
+nums = [1,3,-1,-3,5,3,6,7]
+k = 8
+print(maxSlidingWindow(nums, k))

b4293988b26997a67dd05d4477715ae4d3ddf795
jungle <jungle@alibaba-inc.com>
2019-12-27 21:35:49 +0800

    772 stack

--- demo.py
+++ demo.py
@@ -1,22 +1,57 @@
-#coding=utf-8
-import sys
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
 
-def findTargetSumWays(nums, S):
-    s = sum(nums)
-    if s <S: return 0
-    target,mod = divmod(s+S,2)
-    if mod != 0: return 0
+        def operation(op, second, first):
+            if op == "+":
+                return first + second
+            elif op == "-":
+                return first - second
+            elif op == "*":
+                return first * second
+            elif op == "/":  # integer division
+                return first // second
 
-    dp = [0]*(target+1)
-    dp[0] = 1
-    for n in nums:#嵌套loop的顺序颠倒了，每次累积的数目，会用到后面(全局)的累积数，导致数目倍数增长而偏大
-        for t in range(target, n-1, -1): #该层遍历的顺序如果不倒序，就会导致同一个数字，每个t都会复用t-1的累积数导致结果偏大
-            #避免t-n是负数，或者说t-n是合理的索引，只需设定range(target, n-1, -1)，既可以让t在合理范围内，有可以减少遍历次数
-            if dp[t-n]:
-                dp[t] += dp[t-n]
+        def precedence(current_op, op_from_ops):
+            if op_from_ops == "(" or op_from_ops == ")":
+                return False
+            if (current_op == "*" or current_op == "/") and (op_from_ops == "+" or op_from_ops == "-"):
+                return False
+            return True
 
-    return dp[-1]
+        if not s:
+            return 0
+        # define two stack: nums to store the numbers and ops to store the operators
+        nums, ops = [], []
+        i = 0
+        while i < len(s):
+            c = s[i]
+            if c == " ":
+                i += 1
+                continue
+            elif c.isdigit():
+                num = int(c)
+                while i < len(s) - 1 and s[i + 1].isdigit():
+                    num = num * 10 + int(s[i + 1])
+                    i += 1
+                nums.append(num)
+            elif c == "(":
+                ops.append(c)
+            elif c == ")":
+                # do the math when we encounter a ')' until '('
+                while ops[-1] != "(":
+                    nums.append(operation(ops.pop(), nums.pop(), nums.pop()))
+                ops.pop()
+            elif c in ["+", "-", "*", "/"]:
+                while len(ops) != 0 and precedence(c, ops[-1]):
+                    nums.append(operation(ops.pop(), nums.pop(), nums.pop()))
+                ops.append(c)
+            i += 1
 
-nums = [1, 1, 1, 1, 1]
-S = 3
-print(findTargetSumWays(nums, S))
+        while len(ops) > 0:
+            nums.append(operation(ops.pop(), nums.pop(), nums.pop()))
+
+        return nums.pop()
---
+++ schedule/week-3/Basic Calculator/772. Basic Calculator III.py
@@ -0,0 +1,77 @@
+#coding=utf-8
+
+'''
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string may contain open ( and closing parentheses ),
+the plus + or minus sign -, non-negative integers and empty spaces .
+
+The expression string contains only non-negative integers, +, -, *, / operators ,
+open ( and closing parentheses ) and empty spaces . The integer division should truncate toward zero.
+
+You may assume that the given expression is always valid.
+All intermediate results will be in the range of [-2147483648, 2147483647].
+
+Some examples:
+
+"1 + 1" = 2
+" 6-4 / 2 " = 4
+"2*(5+5*2)/3+(6/2+8)" = 21
+"(2+6* 3+5- (3*14/7+2)*5)+3"=-12
+'''
+
+#stack:
+# 1.遇到数字字符，就累积到num；
+# 2.遇到'('，让前面的那个operator字符入栈，op='+',num=0
+# 3.遇到'+-*/)',就将它前面的num以及num前面的operator一起，
+#   3.1 去更新栈；
+#       3.1.1 如果operator是+-，就让num带上符号，入栈
+#       3.1.2 如果operator是*/，它们计算优先级高，栈顶元素出栈，operator, num，三者一起计算新的值，再入栈
+#   3.2 如果是 ')'，就连续叠加stack栈顶(弹出)元素，直至栈顶元素不是数字(为'('前面的operator)
+# 4.最后将operator,num去更新栈
+
+#KEY：
+# 1.operator 和 num的更新时机(operator 是 num前面的运算符)
+# 2.stack存放待运算的数字和'('之间的operator，每个数字有正负号，便于统一做加法
+# 3.update(op, num)，用于更新栈顶元素
+# 3.最后循环结束之后，需要将最后的operator 和 num，去更新stack栈顶元素
+
+def calculate(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    def update(op, num):
+        if op == '+':
+            stack.append(num)
+        elif op == '-':
+            stack.append(-num)
+        elif op == '*':
+            stack.append(stack.pop()*num)
+        elif op == '/':
+            stack.append(int(float(stack.pop())/num))
+
+    stack = []
+    op, num = '+', 0
+    for c in s:
+        if c.isdigit():
+            num = num*10+int(c)
+        elif c in "+-*/)":
+            update(op, num)
+            if c == ')':
+                num = 0
+                while isinstance(stack[-1], int):
+                    num += stack.pop()
+                update(stack.pop(), num)
+            num, op = 0, c
+        elif c == '(':
+            stack.append(op)
+            num,op = 0,'+'
+
+    update(op, num)
+    return sum(stack)
+
+
+#s = "-1+4*3/3/3"
+s= " ( 2+6 * 3+5- (3*14/7+2)*5)+3 "
+print(calculate(s))

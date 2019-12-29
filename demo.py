32eb4bf3467087bb476238d9e89c11a85830b862
85937023 <85937023@qq.com>
2019-12-29 10:36:51 +0800

    .

--- demo.py
+++ demo.py
@@ -1,57 +1,13 @@
-class Solution(object):
-    def calculate(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+def maxProfit(prices):
+    maxsum, start, end = 0,0,0
+    for i, p in enumerate(prices):
+        if i>0 and p>=prices[i-1]:
+            end = p
+        else:
+            maxsum += end-start
+            start = end = p
+    maxsum += end-start
+    return maxsum
 
-        def operation(op, second, first):
-            if op == "+":
-                return first + second
-            elif op == "-":
-                return first - second
-            elif op == "*":
-                return first * second
-            elif op == "/":  # integer division
-                return first // second
-
-        def precedence(current_op, op_from_ops):
-            if op_from_ops == "(" or op_from_ops == ")":
-                return False
-            if (current_op == "*" or current_op == "/") and (op_from_ops == "+" or op_from_ops == "-"):
-                return False
-            return True
-
-        if not s:
-            return 0
-        # define two stack: nums to store the numbers and ops to store the operators
-        nums, ops = [], []
-        i = 0
-        while i < len(s):
-            c = s[i]
-            if c == " ":
-                i += 1
-                continue
-            elif c.isdigit():
-                num = int(c)
-                while i < len(s) - 1 and s[i + 1].isdigit():
-                    num = num * 10 + int(s[i + 1])
-                    i += 1
-                nums.append(num)
-            elif c == "(":
-                ops.append(c)
-            elif c == ")":
-                # do the math when we encounter a ')' until '('
-                while ops[-1] != "(":
-                    nums.append(operation(ops.pop(), nums.pop(), nums.pop()))
-                ops.pop()
-            elif c in ["+", "-", "*", "/"]:
-                while len(ops) != 0 and precedence(c, ops[-1]):
-                    nums.append(operation(ops.pop(), nums.pop(), nums.pop()))
-                ops.append(c)
-            i += 1
-
-        while len(ops) > 0:
-            nums.append(operation(ops.pop(), nums.pop(), nums.pop()))
-
-        return nums.pop()
+prices = [4,3,8]
+print(maxProfit(prices))
--- schedule/week-1/Best Time to Buy and Sell Stock/122. Best Time to Buy and Sell Stock II.py
+++ schedule/week-1/Best Time to Buy and Sell Stock/122. Best Time to Buy and Sell Stock II.py
@@ -17,12 +17,21 @@ Output: 7
 最native的想法：
     遍历到当前值，判断如果当前元素比前一个大，就记录为当前最大值，继续增长。如果当前元素比前一个小，就马上计算前一个元素的
 收益，并将当前元素记为新一个transactions的最小起点；
-    这里有很多细节去处理，如果当前值不是下降挂点第一个值，而是连续下降值中的某个，那么会让上续判断变得复杂，需要额外增加一个
-变量，记录是否是增长过程中的首次下降，根据这个变量状态，去做分支判断：是否开始计算收益，或者是否只做新transactions的最小起点更新
-
+    
 grace的想法：
     只要价格出现增长，就记录此次增长获得的收益，并入总的收益；如果价格是下降，就不做任何处理。最终累积的收益就是最大收益
 '''
+
+def maxProfit_native(prices):
+    maxsum, start, end = 0,0,0
+    for i, p in enumerate(prices):
+        if i>0 and p>=prices[i-1]:
+            end = p
+        else:
+            maxsum += end-start
+            start = end = p
+    maxsum += end-start
+    return maxsum
 
 def maxProfit(prices):
     profitSum = 0

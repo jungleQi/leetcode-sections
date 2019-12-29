ca9a72416999b795821d810a664ed84404aa36e2
85937023 <85937023@qq.com>
2019-12-29 17:22:02 +0800

    .

--- schedule/week-1/slide window/239. Sliding Window Maximum.py
+++ schedule/week-1/slide window/239. Sliding Window Maximum.py
@@ -10,6 +10,7 @@ Output: [3,3,5,5,6,7]
 # Keep indexes of good candidates in deque d. The indexes in d are from the current window,
 # they're increasing, and their corresponding nums are decreasing.
 # Then the first deque element is the index of the largest window value
+#deque在窗口中的妙用，保持队列中的元素从头到尾是递减，即新来的元素淘汰掉队列尾部小于它的元素，最后新元素入队列
 
 import collections
 def maxSlidingWindow(nums, k):

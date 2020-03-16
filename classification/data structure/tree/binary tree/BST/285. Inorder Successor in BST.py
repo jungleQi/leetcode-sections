b62b5c25ab4296908f5221b1bdbc93d18cc0b206
祁胜军 <jungle@alibaba-inc.com>
2020-03-16 20:32:47 +0800

    285

---
+++ classification/data structure/tree/binary tree/BST/285. Inorder Successor in BST.py
@@ -0,0 +1,32 @@
+'''
+Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+The successor of a node p is the node with the smallest key greater than p.val.
+'''
+
+
+class Solution(object):
+    visitor = False
+
+    def inorderSuccessor(self, root, p):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :rtype: TreeNode
+        """
+        def travel(root, ret):
+            if not root or ret[0]: return
+
+            travel(root.left, ret)
+            if root.val == p.val:
+                self.visitor = True
+
+            if self.visitor and root.val != p.val:
+                ret[0] = root
+                self.visitor = False
+                return
+
+            travel(root.right, ret)
+
+        ret = [None]
+        travel(root, ret)
+        return ret[0]

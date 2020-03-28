0f14b78d14f93cc2fffe7f1e3325510a53e95280
祁胜军 <jungle@alibaba-inc.com>
2020-03-28 18:45:21 +0800

    82

--- classification/data structure/linked list/draft.py
+++ classification/data structure/linked list/draft.py
@@ -1,15 +1,21 @@
 from utils import *
 
 def deleteDuplicates(head):
-    if not head or not head.next: return head
+    if not head or not head.next:
+        return head
 
-    if head.val == head.next.val:
-        head.next = head.next.next
-        deleteDuplicates(head)
-    else:
-        deleteDuplicates(head.next)
+    dummy = ListNode(head.val-1)
+    dummy.next, cur = head, dummy
+    while cur:
+        tail = cur.next
+        while tail and tail.next and tail.val == tail.next.val:
+            tail = tail.next
+        if cur.next != tail:
+            cur.next = tail.next
+        else:
+            cur = cur.next
 
-    return head
+    return dummy.next
 
 arr = []
 mylist = List(arr)
--- classification/data structure/linked list/remove/82. Remove Duplicates from Sorted List II.py
+++ classification/data structure/linked list/remove/82. Remove Duplicates from Sorted List II.py
@@ -1,4 +1,21 @@
-from common import *
+#coding=utf-8
+
+'''
+Given a sorted linked list, delete all nodes that have duplicate numbers,
+leaving only distinct numbers from the original list.
+
+Return the linked list sorted as well.
+
+Example 1:
+Input: 1->2->3->3->4->4->5
+Output: 1->2->5
+
+Example 2:
+Input: 1->1->1->2->3
+Output: 2->3
+'''
+
+from ..utils import *
 
 class ListNode(object):
      def __init__(self, x):
@@ -36,21 +53,28 @@ def deleteDuplicates(head):
     return lnode.next
 
 #very wonderfull
+#1.添加dummy，其值必须不同于第一个节点
+#2.保证cur节点值的唯一性，并且有效移动：
+#  2.1 迭代比较，定位出等值节点区间尾部tail
+#  2.2 如果遇到了等值区间(tail 存在移动，cur.next != tail)，cur保持不变，变的是cur.next
+#  2.3 如果没有遇到等值区间，cur移动一次，变成cur.next
+
 def online_solution(head):
     if not head or not head.next:
         return head
-    curr = prev = ListNode(0)
-    curr.next = head
-    while (head and head.next):
-        if head.val == head.next.val:
-            while (head and head.next and head.val == head.next.val):
-                head = head.next
-            head = head.next
-            prev.next = head
+
+    dummy = ListNode(head.val-1)
+    dummy.next, cur = head, dummy
+    while cur:
+        tail = cur.next
+        while tail and tail.next and tail.val == tail.next.val:
+            tail = tail.next
+        if cur.next != tail:
+            cur.next = tail.next
         else:
-            prev = prev.next
-            head = head.next
-    return curr.next
+            cur = cur.next
+
+    return dummy.next
 
 values = [0,0,1,3,4,4,5,5,5]
 nodes = online_solution(values)

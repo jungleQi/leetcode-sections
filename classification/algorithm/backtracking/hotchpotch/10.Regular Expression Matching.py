ad6e9a0e571c786c88f804d941b52dfb027dddda
祁胜军 <jungle@alibaba-inc.com>
2020-03-28 13:57:39 +0800

    10

--- classification/algorithm/backtracking/hotchpotch/10.Regular Expression Matching.py
+++ classification/algorithm/backtracking/hotchpotch/10.Regular Expression Matching.py
@@ -1,3 +1,5 @@
+#coding=utf-8
+
 '''
 Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
 
@@ -25,4 +27,23 @@ Output: false
 
 '''
 
+#关键是怎么对p中*的处理 ： 对*可能的作用，形成多个分支，构建解空间树
+#难点在于：选择什么时机作为回溯点，如何进行不同分支解的展开，如何结束返回的问题
+#分析:1. '*'只会影响到它前一个字符的行为，不同的影响行为，分别建立一个解分支
+#    2. 如何返回：
+#       s空，p不一定为空也可能是true，判断起来比较麻烦；
+#       p为空，s为空就true，不为空就false。这个得益于isMatch(s, p[2:])
+
 def isMatch(s, p):
+    if not p:
+        return not s
+
+    first_match = bool(s) and p[0] in ('.', s[0])
+    if len(p)>=2 and p[1] == '*':
+        return isMatch(s, p[2:]) or (first_match and isMatch(s[1:], p))
+    else:
+        return first_match and isMatch(s[1:], p[1:])
+
+s = ""
+p = "s*s*i*"
+print(isMatch(s, p))

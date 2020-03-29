fe11f1be059a2ea553ef31ac6b1c4b3357196907
祁胜军 <jungle@alibaba-inc.com>
2020-03-29 11:58:31 +0800

    143

--- classification/data structure/linked list/draft.py
+++ classification/data structure/linked list/draft.py
@@ -1,25 +1,50 @@
 from utils import *
 
-def deleteDuplicates(head):
-    if not head or not head.next:
+def reorderList_recursion(head):
+    def helper(prev, head):
+        if not head or not head.next:
+            return prev
+
+        cur = helper(prev, head.next)
+        if not cur or cur == head or not cur.next:
+            return None
+
+        tmp = cur.next
+        cur.next = head.next
+        head.next.next = tmp
+        head.next = None
+
+        return tmp
+
+    helper(head, head)
+    return head
+
+def reorderList_interator(head):
+    """
+    :type head: ListNode
+    :rtype: None Do not return anything, modify head in-place instead.
+    """
+    if head == None:
         return head
+    temp = head
+    k = 0
+    nodes = list()
+    while temp:
+        nodes.append(temp)
+        temp = temp.next
 
-    dummy = ListNode(head.val-1)
-    dummy.next, cur = head, dummy
-    while cur:
-        tail = cur.next
-        while tail and tail.next and tail.val == tail.next.val:
-            tail = tail.next
-        if cur.next != tail:
-            cur.next = tail.next
-        else:
-            cur = cur.next
+    prev = None
+    for i in range((len(nodes) + 1) // 2):
+        if prev:
+            prev.next = nodes[i]
+        nodes[i].next = nodes[-(i + 1)]
+        prev = nodes[-(i + 1)]
+    prev.next = None
+    return prev
 
-    return dummy.next
-
-arr = []
+arr = [1,2,3]
 mylist = List(arr)
-ret = deleteDuplicates(mylist.head)
+ret = reorderList(mylist.head)
 mylist.printList(ret)
 
 

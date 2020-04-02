961c9c6b6d17e73d0a21ada054f0dbcfe5f9455a
祁胜军 <jungle@alibaba-inc.com>
2020-04-02 14:13:49 +0800

    buy mac

---
+++ classification/data structure/graph/direct graph/990. Satisfiability of Equality Equations.py
@@ -0,0 +1,29 @@
+'''
+Given an array equations of strings that represent relationships between variables,
+each string equations[i] has length 4 and takes one of two different forms: "a==b" or "a!=b".
+Here, a and b are lowercase letters (not necessarily different) that represent one-letter variable names.
+
+Return true if and only if it is possible to assign integers to variable names so as to satisfy all the given equations.
+
+Example 1:
+
+Input: ["a==b","b!=a"]
+Output: false
+Explanation: If we assign say, a = 1 and b = 1, then the first equation is satisfied, but not the second.
+There is no way to assign the variables to satisfy both equations.
+'''
+
+import collections
+def equationsPossible(equations):
+    graph = collections.defaultdict(list)
+    for item in equations:
+        label = 0 if item[1] == '=' else 1
+        graph[item[0]].append([item[3], label])
+
+    visitor = collections.defaultdict(list)
+    def dfs(node, unequationCnt):
+        for nei, label in graph[node]:
+
+            visitor[node] = unequationCnt
+            dfs(nei, unequationCnt+label)
+

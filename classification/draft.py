b99bece07e858e3ddfcb448150a9a7256206a93e
jungleQi <85937023@qq.com>
2020-04-18 10:11:32 +0800

    .

--- classification/draft.py
+++ classification/draft.py
@@ -3,18 +3,30 @@ from utils import Node
 import heapq
 import collections
 
+class UF(object):
+    def __init__(self, N):
+        self.parent = range(N+1)
 
-def findMaxForm(strs, m, n):
-    dp = [[0]*(n+1) for _ in range(m+1)]
-    for str in strs:
-        n0, n1 = str.count('0'), str.count('1')
-        for i in range(m, -1, -1):
-            for j in range(n, -1, -1):
-                if n0 > i or n1>j: break
-                dp[i][j] = max(dp[i][j], dp[i-n0][j-n1]+1)
-    return dp[-1][-1]
+    def find(self, i):
+        if i != self.parent[i]:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
 
-strs = {"10", "0001", "111001", "1", "0"}
-m = 0
-n = 0
-print findMaxForm(strs, m, n)
+    def union(self, i, j):
+        ri, rj = self.find(i), self.find(j)
+        if ri != rj:
+            self.parent[ri] = rj
+            return True
+        else:
+            return False
+
+def findRedundantConnection(edges):
+    N = len(edges)
+    uf = UF(N)
+
+    for edge in edges:
+        if not uf.union(edge[0], edge[1]):
+            return edge
+
+edges = [[1,2], [2,3], [3,4], [1,4], [1,5]]
+print findRedundantConnection(edges)

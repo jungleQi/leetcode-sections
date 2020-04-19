4258d37ec1829899e0af0948ec4537b72872a9cd
jungleQi <85937023@qq.com>
2020-04-19 22:23:11 +0800

    .

--- classification/draft.py
+++ classification/draft.py
@@ -3,18 +3,11 @@ from utils import Node
 import heapq
 import collections
 
-def change(amount, coins):
-    N = len(coins)
-    dp = [[0] * (N + 1) for _ in range(amount + 1)]
-    dp[0][0] = 1
+def lastStoneWeightII(stones):
+    stoneSum = sum(stones)
 
-    for i in range(amount + 1):
-        for j in range(1, N + 1):
-            dp[i][j] = dp[i][j - 1]
-            if i >= coins[j - 1]:
-                dp[i][j] += dp[i - coins[j - 1]][j]
-    return dp[-1][-1]
+    dp = {0}
+    for weight in stones:
+        dp |= {weight+i for i in dp}
 
-coins = [1,2,5]
-amount = 5
-print change(amount, coins)
+    return min([abs(stoneSum-i-i) for i in dp])

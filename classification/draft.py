a763d5ff24e4e13a5e88d564dae1f95f0fa8a74a
jungleQi <85937023@qq.com>
2020-04-20 21:10:35 +0800

    .

--- classification/draft.py
+++ classification/draft.py
@@ -3,36 +3,17 @@ from utils import Node
 import heapq
 import collections
 
-class unionFind(object):
-    def __init__(self, n):
-        self.parent = range(n+1)
+def numRollsToTarget(d, f, target):
+    dp = [1]+[0]*target
 
-    def find(self, i):
-        if i != self.parent[i]:
-            self.parent[i] = self.find(self.parent[i])
-        return self.parent[i]
+    for i in range(1, d + 1):
+        for j in range(target, 0, -1):
+            for k in range(1, f + 1):
+                if k > j: break
+                dp[j] += dp[j-k]
+    return dp[-1] % (10**9 + 7)
 
-    def union(self, i, j):
-        ri, rj = self.find(i), self.find(j)
-        if ri == rj:
-            return False
-        else:
-            self.parent[ri] = rj
-            return True
-
-def minCostToSupplyWater(n, wells, pipes):
-    p = [(c, f, t) for f, t, c in pipes]
-    w = [(c, 0, i) for i, c in enumerate(wells, 1)]
-
-    uf = unionFind(n)
-    ans, edgeCnt = 0, 0
-    for c, f, t in sorted(p+w):
-        if edgeCnt >= n: break
-        if uf.union(f, t):
-            ans += c
-    return ans
-
-n = 3
-wells = [1,2,2]
-pipes = [[1,2,1],[2,3,1]]
-print minCostToSupplyWater(n, wells, pipes)
+d = 30
+f = 30
+target = 500
+print numRollsToTarget(d, f, target)

8173aa93783a0af5c641029b30a7a74600bd0619
jungleQi <85937023@qq.com>
2020-04-26 21:28:32 +0800

    .

--- classification/algorithm/2-dynamic programming/2-linear model/11-300. Longest Increasing Subsequence.py
+++ classification/algorithm/2-dynamic programming/2-linear model/11-300. Longest Increasing Subsequence.py
@@ -1,3 +1,22 @@
+'''
+Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+Example:
+Input: [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+'''
+
+def lengthOfLIS_dp(nums):
+    if not nums: return 0
+    N = len(nums)
+    dp = [1] * N
+    for i in range(1, N):
+        for j in range(i - 1, -1, -1):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return max(dp)
+
 def lengthOfLIS(nums):
     dp = []
     ret = 0
--- classification/draft.py
+++ classification/draft.py
@@ -4,42 +4,19 @@ from utils import ListNode
 import heapq
 import collections
 #
-# d -> 1    -> 2    ->    3  ->   4 ->  5   ->  NULL
-# p   c     n  tmp
-# x <- 1   2  -> 3
-def reverseKGroup(head, k):
-    def _reverse(_head, i):
-        prev, cur, next = None, _head, _head.next
-        while i > 0:
-            cur.next = prev
-            prev = cur
-            cur = next
-            next = next.next
-            i -= 1
-        return cur
 
-    dummy = ListNode(0)
-    dummy.next = head
+def lengthOfLIS(nums):
+    N = len(nums)
+    dp = [1]*N
+    for i in range(1,N):
+        for j in range(i-1, -1, -1):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j]+1)
+    return dp[-1]
 
-    newHead = dummy
-    while newHead:
-
-        #judge if can reverse k nodes
-        i = 1
-        prob = newHead.next
-        while i<k and prob:
-            prob = prob.next
-            i += 1
-        if i != k: break
-
-        #reverve k nodes
-        tail = _reverse(newHead.next, i)
-        tmp = newHead.next
-        newHead.next.next  = prob
-        newHead.next = tail
-        newHead = tmp
-
-    return dummy.next
+#nums = [10,9,2,5,3,7,101,18]
+nums = [1,3,6,7,9,4,10,5,6]
+print lengthOfLIS(nums)
 
 
 

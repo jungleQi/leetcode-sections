d330f15e3102216a4e06453963d64a432fdc6198
jungleQi <85937023@qq.com>
2020-05-01 08:48:18 +0800

    .

--- classification/algorithm/2-dynamic programming/2-linear model/18-740. Delete and Earn.py
+++ classification/algorithm/2-dynamic programming/2-linear model/18-740. Delete and Earn.py
@@ -1,14 +1,27 @@
-#coding=utf-8
-from utils import Node
-from utils import ListNode
-import heapq
+'''
+Given an 7-array nums of integers, you can perform operations on the 7-array.
+
+In each operation, you pick any nums[i] and delete it to earn nums[i] points.
+After, you must delete every element equal to nums[i] - 1 or nums[i] + 1.
+
+You start with 0 points. Return the maximum number of points you can earn by applying such operations.
+
+'''
 import collections
-#
-#        p     c       n
-#   N <- 1  <-   2     NULL
-
-
 def deleteAndEarn(nums):
+    counter = collections.Counter(nums)
+
+    delt, earn, prev = 0, 0, 0
+    for n in sorted(counter):
+        if n != prev+1:
+            delt, earn = max(delt, earn), max(delt, earn) + counter[n]*n
+        else:
+            delt, earn = max(delt, earn), delt+counter[n]*n
+        prev = n
+    return max(delt,earn)
+
+
+def deleteAndEarn_complex_ugly(nums):
     """
     :type nums: List[int]
     :rtype: int
@@ -37,7 +50,5 @@ def deleteAndEarn(nums):
 
     return max(max(earn), max(delete))
 
-#nums = [2, 2, 3, 3, 3, 4]
-nums = [3,3,3,4,2]
-print deleteAndEarn(nums)
-
+nums = [3,3,3,2,4]
+print(deleteAndEarn(nums))

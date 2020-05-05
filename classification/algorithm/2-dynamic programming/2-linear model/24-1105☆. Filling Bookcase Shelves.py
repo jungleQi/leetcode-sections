701e712167dea34d93a094ee1fabfe9c17af70de
jungleQi <85937023@qq.com>
2020-05-05 21:12:04 +0800

    .

--- classification/algorithm/2-dynamic programming/2-linear model/24-1105☆. Filling Bookcase Shelves.py
+++ classification/algorithm/2-dynamic programming/2-linear model/24-1105☆. Filling Bookcase Shelves.py
@@ -1,3 +1,5 @@
+#coding=utf-8
+
 '''
 We have a sequence of books: the i-th book has thickness books[i][0] and height books[i][1].
 
@@ -14,26 +16,32 @@ Note again that at each step of the above process, the order of the books we pla
 Return the minimum possible height that the total bookshelf can be after placing shelves in this manner.
 '''
 
+#lightspot: 阶段没有重新组织划分，还是以原始books数组的每个元素为阶段
+# 但是在当前阶段dp[i]，推演books[i]加入后能获得最小总体高度时，能够推演到最远的那个j，是受限的，
+# 受制于[j..i]累积宽度 不能超过规定的shelft-width。
+
+# 这里给出的启示是：设定的限制条件，可能只是限制了当前dp[i]，向低位推演的条件范围，不影响一般性质的阶段划分和状态转移
+
 import sys
 def minHeightShelves(books, shelf_width):
-    # dp[i] min height after place (i-1)th book
-    # dp[i] = min(curHeight + dp[i-1...0])
     N = len(books)
-    dp = [sys.maxint]*(N+1)
-    dp[0] = 0
 
-    for i in range(1,N+1):
-        width_left, tmp_height = shelf_width, 0
+    # dp[i] min height after place (i-1)th book
+    dp = [0] + [float("inf")] * N
+
+    for i in range(1, N + 1):
+        levelHight, width_left = 0, shelf_width
 
         j = i
-        while j>0:
-            width_left -= books[j-1][0]
-            tmp_height = max(tmp_height, books[j-1][1])
+        while j > 0:
+            width_left -= books[j - 1][0]
+            if width_left < 0:
+                break
 
-            if width_left >= 0:
-                dp[i] = min(dp[i], dp[j-1]+tmp_height)
+            levelHight = max(levelHight, books[j - 1][1])
+            dp[i] = min(dp[i], dp[j - 1] + levelHight)
+
             j -= 1
-
     return dp[-1]
 
 

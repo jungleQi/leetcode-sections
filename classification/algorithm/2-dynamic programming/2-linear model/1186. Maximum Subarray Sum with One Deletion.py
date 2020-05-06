c728ca9b4e7cd945345214a9812284247d1a1cbb
jungleQi <85937023@qq.com>
2020-05-06 22:32:36 +0800

    .

--- classification/algorithm/2-dynamic programming/2-linear model/1186. Maximum Subarray Sum with One Deletion.py
+++ classification/algorithm/2-dynamic programming/2-linear model/1186. Maximum Subarray Sum with One Deletion.py
@@ -1,23 +1,43 @@
+#coding=utf-8
+
+'''
+Given an array of integers, return the maximum sum for a non-empty subarray
+(contiguous elements) with at most one element deletion.
+
+In other words, you want to choose a subarray and optionally delete one element from it so that
+there is still at least one element left and the sum of the remaining elements is maximum possible.
+
+Note that the subarray needs to be non-empty after deleting one element.
+
+Example 1:
+Input: arr = [1,-2,0,3]
+Output: 4
+Explanation:
+Because we can choose [1, -2, 0, 3] and drop -2,
+thus the subarray [1, 0, 3] becomes the maximum value.
+'''
+
+#define state:
+#   delete[i] 到i为止，删除一个元素后的子数组的最大和(可能包含或者不包含i)
+#   keep[i] 到i为止，没删除元素的子数组的最大和(包含i)
 def maximumSum(arr):
     N = len(arr)
-    dp1 = [0]*N
-    dp2 = [0]*N
+    arr = [0] + arr
 
-    maxSum = arr[0]
-    dp1[0] = arr[0]
-    for i in range(1,N):
-        dp1[i] = (dp1[i-1]+arr[i]) if dp1[i-1]>0 else arr[i]
-        maxSum = max(maxSum, dp1[i])
+    delete, keep = [-float("inf")] + [0] * N, [-float("inf")] + [0] * N
+    for i in range(1, N + 1):
+        delete[i] = max(delete[i - 1] + arr[i], keep[i - 1])
+        keep[i] = max(0, keep[i - 1]) + arr[i]
 
-    dp2[N-1] = arr[N-1]
-    for i in range(N-2, -1, -1):
-        dp2[i] = (dp2[i+1]+arr[i]) if dp2[i+1]>0 else arr[i]
+    return max(max(delete), max(keep))
 
-    for i in range(1,N-1):
-        if arr[i] < 0:
-            maxSum = max(maxSum, dp1[i-1]+dp2[i+1])
-
-    return maxSum
+def maximumSum_concise(arr):
+    ret = delete = keep = arr[0]
+    for num in arr[1:]:
+        delete = max(delete + num, keep)
+        keep = max(0, keep) + num
+        ret = max(delete, keep, ret)
+    return ret
 
 arr = [8,-1,6,-7,-4,5,-4,7,-6]
 print(maximumSum(arr))

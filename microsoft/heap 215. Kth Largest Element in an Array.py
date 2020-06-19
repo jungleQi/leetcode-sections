615dde33ed30c9fc1a660ce6f46b44d5537cf4dc
jungleQi <85937023@qq.com>
2020-06-19 17:45:19 +0800

    heap

--- microsoft/heap 215. Kth Largest Element in an Array.py
+++ microsoft/heap 215. Kth Largest Element in an Array.py
@@ -6,3 +6,20 @@ def findKthLargest(nums, k):
     :rtype: int
     """
     return heapq.nlargest(k, nums)[-1]
+
+def findKthLargest_II(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: int
+    """
+
+    heap = []
+    i = 0
+    for num in nums:
+        heapq.heappush(heap, num)
+        i += 1
+        if i > k:
+            heapq.heappop(heap)
+            i -= 1
+    return heap[0]

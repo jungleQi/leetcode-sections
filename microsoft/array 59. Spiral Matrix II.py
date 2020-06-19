ddec3753875658f9a3c420476287e5a3731d512b
jungleQi <85937023@qq.com>
2020-06-19 17:25:47 +0800

    array

---
+++ microsoft/array 59. Spiral Matrix II.py
@@ -0,0 +1,21 @@
+def generateMatrix(n):
+    """
+    :type n: int
+    :rtype: List[List[int]]
+    """
+    ans = [[0] * n for _ in range(n)]
+
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0]
+    di = 0
+    i, j = 0, 0
+    for num in range(1, n * n + 1):
+        ans[i][j] = num
+
+        ni, nj = i + dr[di], j + dc[di]
+        if ni >= n or nj >= n or ni < 0 or nj < 0 or ans[ni][nj] > 0:
+            di = (di + 1) % 4
+
+        i, j = i + dr[di], j + dc[di]
+
+    return ans

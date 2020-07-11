c150ab56e2facd9e3dfad21a55be58ebdc0eb440
jungleQi <85937023@qq.com>
2020-07-12 00:08:01 +0800

    stack

--- classification/data structure/4-stack/394. Decode String.py
+++ classification/data structure/4-stack/394. Decode String.py
@@ -19,21 +19,20 @@ s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
 '''
 
 def decodeString(s):
-    stack = [["", 0]]
-    num = ""
+    stack = [[1, ""]]
+    num = 0
     for c in s:
         if c.isdigit():
-            num += c
+            num = 10 * num + int(c)
         elif c == '[':
-            stack += ["", int(num)],
-            num = ""
+            stack.append([num, ""])
+            num = 0
+        elif c.isalpha():
+            stack[-1][1] += c
         elif c == ']':
-            st, k = stack.pop()
-            stack[-1][0] += st * k
-        else:
-            stack[-1][0] += c
-
-    return stack[0][0]
+            item = stack.pop()
+            stack[-1][1] += item[1] * item[0]
+    return stack[0][1]
 
 s = "3[a]2[b4[F]c]"
 print(decodeString(s))

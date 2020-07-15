0bea49608951faf5196d74e192ae8566df9e7402
jungleQi <85937023@qq.com>
2020-07-15 22:55:04 +0800

    stack

---
+++ classification/data structure/4-stack/1209. Remove All Adjacent Duplicates in String II.py
@@ -0,0 +1,13 @@
+'''
+Given a string s, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them causing the left and the right side of the deleted substring to concatenate together.
+We repeatedly make k duplicate removals on s until we no longer can.
+Return the final string after all such duplicate removals have been made.
+It is guaranteed that the answer is unique.
+
+Example 1:
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+'''
+
+def removeDuplicates(s, k):
--- classification/data structure/4-stack/173. Binary Search Tree Iterator.py
+++ classification/data structure/4-stack/173. Binary Search Tree Iterator.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 
 '''
-Implement an iterator over a 6-binary 6-search 3-tree (BST).
+Implement an iterator over a binary search tree (BST).
 Your iterator will be initialized with the root node of a BST.
 
 Calling next() will return the next smallest number in the BST.
@@ -9,7 +9,7 @@ Calling next() will return the next smallest number in the BST.
 Note:
 
 next() and hasNext() should run in average O(1) time and uses O(h) memory,
-where h is the height of the 3-tree.
+where h is the height of the tree.
 
 You may assume that next() call will always be valid, that is,
 there will be at least a next smallest number in the BST when next() is called.

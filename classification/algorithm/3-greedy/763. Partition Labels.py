b7553a363f7afdff4450b684739c5917aad61dc9
jungleQi <85937023@qq.com>
2020-08-12 23:12:14 +0800

    greedy

--- classification/algorithm/3-greedy/763. Partition Labels.py
+++ classification/algorithm/3-greedy/763. Partition Labels.py
@@ -1,14 +1,27 @@
+'''
+A string S of lowercase English letters is given.
+We want to partition this string into as many parts as possible so that each letter appears in at most one part,
+and return a list of integers representing the size of these parts.
+
+Example 1:
+Input: S = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
+'''
 
 def partitionLabels(S):
-    last = {c:i for i,c in enumerate(S)}
-
-    anchor,j = 0,0
+    cdict = {c: i for i, c in enumerate(S)}
     ret = []
-    for i,c in enumerate(S):
-        j = max(j, last[c])
-        if i == j:
-            ret.append(j-anchor+1)
-            anchor = j+1
+    largestIdx = anchor = 0
+    for i, c in enumerate(S):
+        if cdict[c] == i and i >= largestIdx:
+            ret.append(i - anchor + 1)
+            anchor = i + 1
+        largestIdx = max(largestIdx, cdict[c])
     return ret
 
 s = "ababcbacadefegdehijhklij"
--- classification/draft.py
+++ classification/draft.py
@@ -1,15 +1,13 @@
-# T = [73, 74, 75, 71, 69, 72, 76, 73]
-def dailyTemperatures(T):
-    N = len(T)
-    stack = []
-    ans = [0]*N
-    for i,n in enumerate(T):
-        while stack and T[stack[-1]]<n:
-            idx = stack.pop()
-            ans[idx] = i-idx
-        stack.append(i)
-    return ans
+def partitionLabels(S):
+    cdict = {c:i for i,c in enumerate(S)}
+    ret = []
+    largestIdx = anchor = 0
+    for i, c in enumerate(S):
+        if cdict[c] == i and i >= largestIdx:
+            ret.append(i-anchor+1)
+            anchor = i+1
+        largestIdx = max(largestIdx, cdict[c])
+    return ret
 
-T = []
-print(dailyTemperatures(T))
-
+S = "ababcbacadefegdehijhklij"
+print(partitionLabels(S))

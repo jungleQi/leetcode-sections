59e8683c140a3205030be10f23d33ad986518122
jungleQi <85937023@qq.com>
2020-09-15 23:10:16 +0800

    greedy

--- classification/algorithm/3-greedy/646-A. Maximum Length of Pair Chain.py
+++ classification/algorithm/3-greedy/646-A. Maximum Length of Pair Chain.py
@@ -6,6 +6,15 @@ Chain of pairs can be formed in this fashion.
 
 Given a set of pairs, find the length longest chain which can be formed. You needn't use up all the given pairs.
 You can select pairs in any order.
+
+Example 1:
+Input: [[1,2], [2,3], [3,4]]
+Output: 2
+Explanation: The longest chain is [1,2] -> [3,4]
+
+Note:
+The number of given pairs will be in the range [1, 1000].
+
 '''
 
 def findLongestChain(pairs):
@@ -15,6 +24,23 @@ def findLongestChain(pairs):
             curTail = pair[1]
             pairCnt += 1
     return pairCnt
+
+def findLongestChain_FromLeftToRight(pairs):
+    """
+    :type pairs: List[List[int]]
+    :rtype: int
+    """
+    pairs.sort(key=lambda x: x[0])
+
+    chainCnt = 0
+    leftest = pairs[0][0] - 1
+    for pair in pairs:
+        if pair[0] > leftest:
+            chainCnt += 1
+            leftest = pair[1]
+        else:
+            leftest = min(leftest, pair[1])
+    return chainCnt
 
 pairs = [[1,2], [2,3], [3,4]]
 print(findLongestChain(pairs))

49fe58ca32790e50641ba521a792fac85a92f580
jungleQi <85937023@qq.com>
2020-09-17 20:59:05 +0800

    .

---
+++ classification/algorithm/3-greedy/738-S. Monotone Increasing Digits.py
@@ -0,0 +1,55 @@
+'''
+Given a non-negative integer N, find the largest number that is less than or equal to N with monotone increasing digits.
+
+(Recall that an integer has monotone increasing digits if and only if each pair of adjacent digits x and y satisfy x <= y.)
+
+Example 1:
+Input: N = 10
+Output: 9
+
+Input: N = 332
+Output: 299
+'''
+
+def monotoneIncreasingDigits_latest(N):
+    digStr = list(str(N))
+    start = 0
+    for i in range(1, len(digStr)):
+        if digStr[i]>digStr[i-1]:
+            start = i
+        elif digStr[i]<digStr[i-1]:
+            break
+
+    if start == len(digStr)-1:
+        return N
+    else:
+        if digStr[start] == '1':
+            return int('9'*(len(digStr)-start-1))
+        else:
+            digStr[start] = str(int(digStr[start])-1)
+            return int("".join(digStr[:start+1]) + '9'*(len(digStr)-start-1))
+
+
+def monotoneIncreasingDigits(N):
+    arr = [int(c) for c in str(N)]
+    nLen = len(arr)
+
+    curidx,latestConvertIdx = 0, nLen
+    for curidx in range(nLen-2, -1, -1):
+        if arr[curidx] > arr[curidx+1]:
+            latestConvertIdx = curidx+1
+            arr[curidx] = arr[curidx]-1
+
+    resArr = [9]*(nLen-latestConvertIdx)
+    if arr[curidx] != 0:
+        res = arr[:latestConvertIdx] + resArr
+    else:
+        res = resArr
+
+    ret = [str(i) for i in res]
+    return int("".join(ret)) if ret else 0
+
+
+
+N = 2220 #101
+print monotoneIncreasingDigits(N)
--- classification/draft.py
+++ classification/draft.py
@@ -1,13 +1,24 @@
-def partitionLabels(S):
-    cdict = {c:i for i,c in enumerate(S)}
-    ret = []
-    largestIdx = anchor = 0
-    for i, c in enumerate(S):
-        if cdict[c] == i and i >= largestIdx:
-            ret.append(i-anchor+1)
-            anchor = i+1
-        largestIdx = max(largestIdx, cdict[c])
-    return ret
+def monotoneIncreasingDigits(N):
+    digStr = list(str(N))
+    start = 0
+    for i in range(1, len(digStr)):
+        if digStr[i]>digStr[i-1]:
+            start = i
+        elif digStr[i]<digStr[i-1]:
+            break
 
-S = "ababcbacadefegdehijhklij"
-print(partitionLabels(S))
+    if start == len(digStr)-1:
+        return N
+    else:
+        if digStr[start] == '1':
+            return int('9'*(len(digStr)-start-1))
+        else:
+            digStr[start] = str(int(digStr[start])-1)
+            return int("".join(digStr[:start+1]) + '9'*(len(digStr)-start-1))
+
+# 10  9
+# 123 123
+# 3306 299
+
+N = 1008
+print(monotoneIncreasingDigits(N))

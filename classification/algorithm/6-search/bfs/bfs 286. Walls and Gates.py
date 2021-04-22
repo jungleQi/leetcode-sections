451313191b5e5e89bca3e08be0c5a9d2169c214c
jungleQi <85937023@qq.com>
2021-04-22 09:40:55 +0800

    ..

--- classification/algorithm/6-search/bfs/bfs 286. Walls and Gates.py
+++ classification/algorithm/6-search/bfs/bfs 286. Walls and Gates.py
@@ -21,6 +21,7 @@ def wallsAndGates(rooms):
                 rooms[x + nei[0]][y + nei[1]] = d + 1
                 deque.append((x + nei[0], y + nei[1], d + 1))
 
+
     for i in range(M):
         for j in range(N):
             if rooms[i][j] == 0:

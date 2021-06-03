9141fc25296af0fb31f8aa13002eb36570e5a1c1
jungleQi <85937023@qq.com>
2021-06-03 11:06:39 +0800

    84

--- classification/data structure/4-stack/84. Largest Rectangle in Histogram.py
+++ classification/data structure/4-stack/84. Largest Rectangle in Histogram.py
@@ -3,4 +3,16 @@ Given n non-negative integers representing the histogram's bar height where the 
 '''
 
 def largestRectangleArea(heights):
-    
+    heights.append(0)
+    stack = [-1]
+    res = 0
+    for i in range(len(heights)):
+        while heights[i] < heights[stack[-1]]:
+            h = heights[stack.pop()]
+            w = i - stack[-1] - 1
+            res = max(res, h * w)
+        stack.append(i)
+    return res
+
+arr = [2,1,5,6,2,3]
+print(largestRectangleArea(arr))

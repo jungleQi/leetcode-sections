d173d8477e51761616057ea01223f014ae152395
jungleQi <85937023@qq.com>
2021-07-06 14:42:01 +0800

    881

--- classification/algorithm/3-greedy/881. Boats to Save People.py
+++ classification/algorithm/3-greedy/881. Boats to Save People.py
@@ -9,6 +9,10 @@ Output: 1
 Explanation: 1 boat (1, 2)
 '''
 
+# people[0, 1, ... k-1, k]，体重按升序排列。
+# 结论一：如果people[k]都能将people[i]带走，那么people[j]更能将people[i]带走，所以将people[i]留给比people[k]体重小的带走，其中(i < j < k)
+# 推论二：逆序遍历，每走一个就带一个当前最轻的走
+
 def numRescueBoats(people, limit):
     people.sort()
     i, j = 0, len(people) - 1
@@ -22,4 +26,4 @@ def numRescueBoats(people, limit):
 
 people =[3]
 limit = 4
-print numRescueBoats(people, limit)
+print(numRescueBoats(people, limit))

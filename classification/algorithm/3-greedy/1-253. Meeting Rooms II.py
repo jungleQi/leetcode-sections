6f42ce446a787a61172c24546357a20e28eb4843
jungleQi <85937023@qq.com>
2021-07-16 16:12:00 +0800

    253

--- classification/algorithm/3-greedy/1-253. Meeting Rooms II.py
+++ classification/algorithm/3-greedy/1-253. Meeting Rooms II.py
@@ -17,6 +17,9 @@ def minMeetingRooms(intervals):
 
     heap = []
     for interval in intervals:
+        # 为何贪婪的要删除掉最小(最左端)的，因为这样处理起来简单；
+        # 也可以删除和当前interval[0]最左邻近的值，只是这样处理起来比较麻烦，还要拿着interval[0]去找比它小的最大那个值
+        # 最后效果和直接删最小的值一样
         if heap and interval[0] >= heap[0]:
             heapq.heappop(heap)
         heapq.heappush(heap, interval[1])
